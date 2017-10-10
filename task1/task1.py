bd899fd1ae4b2fd656ef58535063872c1fde5014
Shane Fay <faysh@tcd.ie>
2017-10-10 11:35:21 +0100

    stuff

--- task1/task1.py
+++ task1/task1.py
@@ -1,19 +1,21 @@
-import plotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 import csv
 import os.path
+import pandas as pd
 
 basepath = os.path.abspath(os.path.dirname(__file__))
 path = os.path.abspath(os.path.join(basepath, "..", "ML_data/SUM_noise.csv"))
-#with open(path) as f:
-#    test = [line for line in csv.reader(f)]
 
-sum_noise = open(path, 'r')
+
+data = pd.read_csv(path, sep=";")
+
 #figure out if we need feature 5
-sum_noise_data = np.loadtxt(sum_noise, dtype='int', delimiter=';', usecols=(1,2,3,4,6,7,8,9,10), skiprows=1)
-sum_noise_targets = np.loadtxt(sum_noise, dtype='int', delimiter=';', usecols=(11), skiprows=1)
+sum_noise_data = data.iloc[:,[1,2,3,4,6,7,8,9,10]]
+sum_noise_targets = data.iloc[:,11]
+
 chunk_size = [100, 500, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000, 10000000, 50000000, 100000000]
 
 sum_X = sum_noise_data
@@ -27,14 +29,15 @@ sum_y_train = sum_noise_targets[:100]
 sum_y_test = sum_noise_targets[-100:]
 
 
+
+
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
 regr.fit(sum_X_train, sum_y_train)
 
 # Make predictions using the testing set
-sum_y_pred = regr.predict(sum_X_test)	
-
+sum_y_pred = regr.predict(sum_X_test)
 
 
 # The coefficients
@@ -45,9 +48,9 @@ print("Mean squared error: %.2f"
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.2f' % r2_score(sum_y_test, sum_y_pred))
 
-# Plot outputs
-plt.scatter(sum_X_test, sum_y_test,  color='black')
-plt.plot(sum_X_test, sum_y_pred, color='blue', linewidth=3)
+# Plot outputs	
+plt.scatter(sum_X_test.iloc[0], sum_y_test,  color='black')
+plt.plot(sum_X_test.iloc[0], sum_y_pred, color='blue', linewidth=3)
 
 plt.xticks(())
 plt.yticks(())
